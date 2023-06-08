d19989a61328dfe958909bb5a938fdafeacff576
misaki04 <misakichi0043@gmail.com>
2023-06-08 10:00:59 +0900

    ✨ 練習と動作確認の為

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Counter } from "@/src/components/Counter";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <h1>チョコレート検定</h1> */}
+      {/* <Counter /> */}
+    </div>
+  );
+}
