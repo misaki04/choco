b2d78e35df9975915794477d5c65b277d9099a87
misaki04 <misakichi0043@gmail.com>
2023-06-08 17:17:53 +0900

    ✨ chocolateのpage作成

---
+++ app/overview/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import styles from "./overview.module.css";
+const page = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h2>概要欄</h2>
+      <nav>
+        {/* <ul className={styles.ul}>
+          <li className={styles.li}> スペシャリスト（初級）</li>
+          <li className={styles.li}> エキスパート（中級）</li>
+          <li className={styles.li}> プロフェッショナル（上級）</li>
+        </ul> */}
+      </nav>
+    </div>
+  );
+};
+
+export default page;
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,24 @@
 import { Counter } from "@/src/components/Counter";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Cacao from "../app/images/Cacao2.jpg";
+import Cacao1 from "../app/images/Cacao1.jpg";
 
 export default function Home() {
   return (
-    <div>
-      {/* <h1>チョコレート検定</h1> */}
-      {/* <Counter /> */}
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <Image src={Cacao} alt="カカオ" />
+        {/* <img src="images/Cacao2.icon" alt="カカオ" /> */}
+      </header>
+      <div className={styles.title}>
+        <h1 className={styles.name}>チョコレート検定</h1>
+        <Image className={styles.img} src={Cacao1} alt="チョコレート" />
+      </div>
+      <Link className={styles.button} href={"../overview"}>
+        START
+      </Link>
     </div>
   );
 }
