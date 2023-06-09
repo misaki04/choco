66454407d41f9fd436da78e30c6b06f35b51c73f
misaki04 <misakichi0043@gmail.com>
2023-06-09 23:45:00 +0900

    🐛不要な記述削除

--- app/overview/page.jsx
+++ app/overview/page.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import styles from "./page.module.css";
-const page = (props) => {
-  console.log(props);
-
+const page = () => {
   return (
     <div>
       <h2 className={styles.name}>概要欄</h2>
       <ul className={styles.ul}>
+        {/* map関数 */}
         <li className={styles.li}> スペシャリスト（初級）</li>
         <li className={styles.li}> エキスパート（中級）</li>
         <li className={styles.li}> プロフェッショナル（上級）</li>
       </ul>
-      {/* <button className={stylse.button}>ul</button> */}
     </div>
   );
 };
