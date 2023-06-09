cc41826a7ca559712ee2515874299a618cb0469b
misaki04 <misakichi0043@gmail.com>
2023-06-09 18:07:26 +0900

    ✨ 概要欄　文字の配置と大きさ調整

--- app/overview/page.jsx
+++ app/overview/page.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-// import styles from "./overview.module.css";
+import styles from "./page.module.css";
 const page = (props) => {
   console.log(props);
 
   return (
     <div>
-      <h2>概要欄</h2>
-      <nav>
-        {/* <ul className={styles.ul}>
-          <li className={styles.li}> スペシャリスト（初級）</li>
-          <li className={styles.li}> エキスパート（中級）</li>
-          <li className={styles.li}> プロフェッショナル（上級）</li>
-        </ul> */}
-      </nav>
+      <h2 className={styles.name}>概要欄</h2>
+      <ul className={styles.ul}>
+        <li className={styles.li}> スペシャリスト（初級）</li>
+        <li className={styles.li}> エキスパート（中級）</li>
+        <li className={styles.li}> プロフェッショナル（上級）</li>
+      </ul>
+      {/* <button className={stylse.button}>ul</button> */}
     </div>
   );
 };
