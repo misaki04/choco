0063ac14d5e514423b56887123b380b41dfd86f7
misaki04 <misakichi0043@gmail.com>
2023-06-11 22:31:10 +0900

    📝 ヘッダーファイル移動とその他は文章を追加作成

--- app/contents/[id]/page.jsx
+++ app/contents/[id]/page.jsx
@@ -1,13 +1,27 @@
-import Header from "@/app/components/Header";
 import React from "react";
+import Header from "@/app/components/Header";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 const page = (props) => {
   console.log("propsで運ばれたものだよ", props);
   return (
     <div>
-      <Header title={"aaa"} />
-      <p>bbbb</p>
-      <p>aaa</p>
+      <Header title={"スペシャリスト(初級)"} />
+      <h3 className={styles.contents}>出題内容</h3>
+
+      <p className={styles.text}>
+        生産現場から最終商品に至る、カカオや
+        <br />
+        チョコレートに関する幅広い知識を習得
+        <br />
+        し、自らのチョコレートの世界を広げる
+        <br />
+        ことができる方向けの初級レベル。
+      </p>
+      <Link href="/" className={styles.button}>
+        20 問
+      </Link>
     </div>
   );
 };
