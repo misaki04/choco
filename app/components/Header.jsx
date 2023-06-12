72c84231b8b57ea3bbbb51921340fce97c0c0c5e
misaki04 <misakichi0043@gmail.com>
2023-06-12 21:34:31 +0900

    ✨ return新機能追加

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./header.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Return from "../images/Return.jpg";
 
 const Header = (props) => {
-  return <h2 className={styles.names}>{props.title}</h2>;
+  return (
+    <div>
+      <h2 className={styles.names}>
+        <Link href="" className={styles.returnImg}>
+          <Image className={styles.returnImg} src={Return} alt="戻るボタン" />
+        </Link>
+        {props.title}
+      </h2>
+    </div>
+  );
 };
 
 export default Header;
