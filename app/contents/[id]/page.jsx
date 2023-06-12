334585eae1caeb160dbe782908f1cb07247d41b4
misaki04 <misakichi0043@gmail.com>
2023-06-12 22:31:01 +0900

    ✨チョコレート検定の出題内容を追加

--- app/contents/[id]/page.jsx
+++ app/contents/[id]/page.jsx
@@ -3,11 +3,16 @@ import Header from "@/app/components/Header";
 import styles from "./page.module.css";
 import Link from "next/link";
 
-const page = () => {
+const ITEMS = [
+  { title: "スペシャリスト(初級)", contents: "出題内容" },
+  { title: "エキスパート(中級)", contents: "出題内容" },
+  { title: "プロフェッショナル(上級)", contents: "出題内容" },
+];
+function page() {
   return (
     <div>
-      <Header title={"スペシャリスト(初級)"} />
-      <h3 className={styles.contents}>出題内容</h3>
+      <Header title={styles.title} />
+      <h3 className={styles.contents}></h3>
 
       <p className={styles.text}>
         生産現場から最終商品に至る、カカオや
@@ -21,8 +26,43 @@ const page = () => {
       <Link href="/" className={styles.button}>
         20 問
       </Link>
+
+      <Header title={styles.title} />
+      <h3 className={styles.contents}></h3>
+
+      <p className={styles.text}>
+        カカオやチョコレートに関する幅広い
+        <br />
+        知識をもとに、友人、知人など自らが
+        <br />
+        所属するコミュニティーにチョコレート
+        <br />
+        の楽しみの世界を広げる活動を行うこと
+        <br />
+        ができる方向けの中級レベル。
+      </p>
+      <Link href="/" className={styles.button}>
+        20 問
+      </Link>
+      <Header title={styles.title} />
+      <h3 className={styles.contents}></h3>
+
+      <p className={styles.text}>
+        カカオやチョコレートに関する幅広く、
+        <br />
+        専門的な知識を持ち、自らチョコレート
+        <br />
+        文化を世の中に発信できる。
+        <br />
+        チョコレート好きの頂点を目指す方向け
+        <br />
+        の上級レベル。
+      </p>
+      <Link href="/" className={styles.button}>
+        20 問
+      </Link>
     </div>
   );
-};
+}
 
 export default page;
